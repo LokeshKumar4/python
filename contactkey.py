df4eaf4a213ae655dcca1fb70021faea211c7e02
LOKESH KUMAR <lokesh.kumar@futurensetech.com>
2023-02-08 08:26:40 +0530

    creating EC2 instances and stopping using python script

--- contactkey.py
+++
@@ -1,5 +0,0 @@
-
-vals= {
-        "access_key":"AKIA44JD6V7A2KM4KL5R",
-        "secret_access_key":"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"
-        }
--- launchEC2.py
+++ launchEC2.py
@@ -1,11 +1,11 @@
 import boto3
-import contactkey
+import contactkeys
 
 
 ec2=boto3.client('ec2',
 			'us-east-1',
-			aws_access_key_id=contactkey.vals["access_key"],
-			aws_secret_access_key=contactkey.vals["secret_access_key"])
+			aws_access_key_id=contactkeys.vals["access_key"],
+			aws_secret_access_key=contactkeys.vals["secret_access_key"])
 
 conn=ec2.run_instances(
                 InstanceType="t2.micro",
